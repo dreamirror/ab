849beb718941c011e9eb54c4fa5c7684aced00c3
zhourui@digisky.com <zhourui@digisky.com>
2018-10-30 15:28:41 +0800

    ab的加载使用

--- Script/Main.cs
+++ Script/Main.cs
@@ -2,37 +2,43 @@
 using System.Collections.Generic;
 using UnityEngine;
 using DigiSky.AssetBundleKit;
-
+using UnityEngine.UI;
 public class Main : MonoBehaviour {
-    public Sprite imgS;     //先创建个Sprite  设置上图片 
-                            // Use this for initialization
+    public GameObject myGgameObj;     //先创建个Sprite  设置上图片 
+    public Image image;            // Use this for initialization
+
     void Start () {
 
 
 	}
     public System.Action<Object> call;
 
-    public static void Call_back(Object obj) {
+    public void Call_back(Object obj) {
         Debug.Log("call back");
-
+        //Object pic = obj.LoadAsset("1.jpg");
+        var texture = obj as Texture2D;
+        Sprite sp = Sprite.Create(texture, new Rect(0, 0, 2048, 1024), Vector2.zero);
+        if (sp != null) {
+            Debug.Log("******************");
+        }
+        image.sprite = sp;
     }
-   
-
 
     private void Awake()
     {
         // 创建GameObject对象                 
 
-        GameObject gameObj = GameObject.Find("Cube");
+        GameObject gameObj = GameObject.Find("Image");
+        image = gameObj.GetComponent<Image>();
+        if (image != null) {
+            Debug.Log("@@@@@@@@@@@@@");
+        }
         System.Action<Object> calls = Call_back;
         AssetBundleManager.Initialization();
         AssetBundleManager mgr = AssetBundleManager.GetSingel();
         mgr.InitManifest();
-        mgr.LoadAssetAsyc("Assets/StreamingAssets/assetbundles/","pic", calls);
-        UnityEngine.Object pic = mgr._LoadSingleAssetInternal("Assets/StreamingAssets/assetbundles/pic/","bg/2.jpg");
-        if (pic != null) {
-            Debug.Log("pic is not null");
-        }
+        mgr.LoadAssetAsyc("Assets/StreamingAssets/assetbundles/pic/bg/","1", calls);
+        //UnityEngine.Object pic = mgr._LoadSingleAssetInternal("Assets/StreamingAssets/assetbundles/pic/","2.jpg");
     }
 
 
